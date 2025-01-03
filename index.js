3cefb046ed0ff7574e0593297adee57dadb4fcc7
Ishita <ishitajain385@gmail.com>
2025-01-03 22:58:22 +0530

    wordsfound for filter

--- index.js
+++ index.js
@@ -4,23 +4,45 @@ let arrayofAuthors= []
 let arrayofImages=[]
 let arrayofDescription =[]
 let originalContent;
-async function getNews(){
-    let data = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-12-03&sortBy=publishedAt&apiKey=4648b16c1c2e4f258894c57786439837')
-    let res= await data.json()
+let wordsFound= []
+let value="";
+let res;
 
-    for(let i=0; i<res.articles.length;i++){
+async function getNews(){
+    wordsFound=[]
+    let data = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-12-03&sortBy=publishedAt&apiKey=4648b16c1c2e4f258894c57786439837')
+     res= await data.json()
+
+     for(let i=0; i<res.articles.length;i++){
         arrayofNews.push(res.articles[i].title)
         arrayofAuthors.push(res.articles[i].author)
         arrayofImages.push(res.articles[i].urlToImage)
         arrayofDescription.push(res.articles[i].description)
+       
     }
-    console.log(arrayofImages)
-    console.log(res.articles)
-    console.log("list of titles")
+
     let loading= document.querySelector('.loading')
     loading.remove()
+    console.log("arrayofimages",arrayofImages)
+   
+   creatingNodes()
+    
+    
+}
+
+getNews()
+
+
+function creatingNodes(val){
+  wordsFound= []
   
     for(let i=0; i<arrayofNews.length;i++){
+        if(val !== "" && arrayofNews[i].includes(val)){
+
+    wordsFound.push(arrayofNews[i])
+
+
+        }
         if(arrayofNews[i] !="[Removed]"){
             let div= document.createElement('p')
             let button= document.createElement('button')
@@ -37,7 +59,7 @@ async function getNews(){
     button.addEventListener("click",()=>{
         openNews(i)
     })
-            div.textContent = arrayofNews[i].substring(0,100)+"..."
+            div.textContent = i,arrayofNews[i].substring(0,100)+"..."
            div.prepend(img)
         div.append(button,span)
           document.querySelector('div').append(div)
@@ -47,9 +69,8 @@ async function getNews(){
 
     }
     originalContent = document.body.cloneNode(true);
+    console.log("words::",wordsFound)
 }
-
-getNews()
 
 function openNews(index){
   let htmldocu = `
@@ -72,8 +93,22 @@ console.log(arrayofDescription[index])
 
 function goBack(){
 document.body.innerHTML= originalContent.innerHTML
-getNews()
+
 }
+
+let ul =document.querySelector("ul")
+
+document.querySelector('input').addEventListener("input",(e)=>{
+    value= e.target.value
+    console.log(value)
+  creatingNodes(value)
+
+})
+
+
+
+
+
 
 
 
