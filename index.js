1ab6a25f21060dbacffe8f4784cb960978262548
Ishita <ishitajain385@gmail.com>
2025-01-04 21:40:47 +0530

    latest, filter works

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let arrayofNews= []
 let arrayofAuthors= []
 let arrayofImages=[]
@@ -6,13 +5,12 @@ let arrayofDescription =[]
 let originalContent;
 let wordsFound= []
 let value="";
-let res;
 
 async function getNews(){
-    wordsFound=[]
-    let data = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-12-03&sortBy=publishedAt&apiKey=4648b16c1c2e4f258894c57786439837')
-     res= await data.json()
-
+   
+    let data = await fetch('https://newsapi.org/v2/everything?q=apple&from=2025-01-03&to=2025-01-03&sortBy=popularity&apiKey=4648b16c1c2e4f258894c57786439837' )
+   let res= await data.json()
+console.log(res)
      for(let i=0; i<res.articles.length;i++){
         arrayofNews.push(res.articles[i].title)
         arrayofAuthors.push(res.articles[i].author)
@@ -22,7 +20,7 @@ async function getNews(){
     }
 
     let loading= document.querySelector('.loading')
-    loading.remove()
+   if(loading) loading.remove()
     console.log("arrayofimages",arrayofImages)
    
    creatingNodes()
@@ -30,52 +28,55 @@ async function getNews(){
     
 }
 
-getNews()
 
 
-function creatingNodes(val){
-  wordsFound= []
-  
-    for(let i=0; i<arrayofNews.length;i++){
-        if(val !== "" && arrayofNews[i].includes(val)){
-
-    wordsFound.push(arrayofNews[i])
-
-
-        }
-        if(arrayofNews[i] !="[Removed]"){
-            let div= document.createElement('p')
-            let button= document.createElement('button')
-            let span= document.createElement('span')
-            span.textContent="~"+arrayofAuthors[i]
-            let img= document.createElement('img')
-            img.src=  arrayofImages[i]
-           
-            if(arrayofImages[i] ==null){
-               img.style.display ="none"
-            }
-            
-    button.textContent="Read More.."
-    button.addEventListener("click",()=>{
-        openNews(i)
-    })
-            div.textContent = i,arrayofNews[i].substring(0,100)+"..."
-           div.prepend(img)
-        div.append(button,span)
-          document.querySelector('div').append(div)
-        }
-       
+    function creatingNodes(val = "") {
+      const contentDiv = document.querySelector('div');
+      contentDiv.innerHTML = ""; 
      
+        wordsFound = arrayofNews
+          .map((news, index) => ({ title: news, index })) 
+          .filter(item => item.title.toLowerCase().includes(val.toLowerCase())); 
+    
+        if (wordsFound.length === 0) {
+          contentDiv.textContent = "No results found for your search.";
+          return;
+        }
+     
+        
+       
+      
+    console.log(wordsFound)
+      
+      wordsFound.forEach(({ title, index }) => {
+        const div = document.createElement('p');
+        const button = document.createElement('button');
+        const span = document.createElement('span');
+        const img = document.createElement('img');
+    
+        img.src = arrayofImages[index] || "";
+        if(img.src ===""){
+          img.style.display="none"        }
+    
+        span.textContent = "~" + (arrayofAuthors[index] || "Unknown Author");
+        button.textContent = "Read More..";
+        button.addEventListener("click", () => openNews(index));
+    
+        div.textContent = title.substring(0, 100) + "...";
+        div.prepend(img);
+        div.append(button, span);
+        contentDiv.append(div);
+      });
 
+      if (!originalContent) originalContent = document.body.innerHTML;
     }
-    originalContent = document.body.cloneNode(true);
-    console.log("words::",wordsFound)
-}
+    
+
+
 
 function openNews(index){
-  let htmldocu = `
-  
-<img src=${arrayofImages[index]} style="padding-top:50px">
+  let htmldocu = ` 
+  <img src=${arrayofImages[index]} style="padding-top:50px">
 <span style="margin-left:370px ; font-size:25px; width:700px"> ${arrayofNews[index]}</span>
 <span style="padding-left:900px ; padding-top:20px">  ~ ${arrayofAuthors[index]}</span>
 <div style="width:600px ; font-size:22px ; margin-right:550px"> 
@@ -86,17 +87,20 @@ function openNews(index){
     </div>
 <button style="border:1px solid black; padding:10px;margin-left:340px" onClick="goBack()"> back </button>
   `
+  
+
+  
  document.body.innerHTML = htmldocu 
 
 console.log(arrayofDescription[index])
 }
 
 function goBack(){
-document.body.innerHTML= originalContent.innerHTML
+document.body.innerHTML= originalContent
 
 }
 
-let ul =document.querySelector("ul")
+
 
 document.querySelector('input').addEventListener("input",(e)=>{
     value= e.target.value
@@ -107,6 +111,7 @@ document.querySelector('input').addEventListener("input",(e)=>{
 
 
 
+getNews()
 
 
 
